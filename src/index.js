6560b5953fa05c23a09fd14e49f9c3c7a9415b28
Kingsley Victor <javaprodigy56@gmail.com>
2021-10-31 19:04:15 +0100

    Add SEO

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import SEO from "./seo";
 import App from "./App";
 
 const MainComponent = () => (
   <div>
-    <Helmet>
-      <meta
-        name="description"
-        content="The VEFI Foundation is an independent standard body that controls and oversees the advancement of the VEFI Network and ecosystem. We are the owner of the VEFI brand. The foundation works to drive the adoption and partnerships, grow the VEFI community."
-      />
-      <title>The VEFI Foundation</title>
-    </Helmet>
+    <SEO
+      title="The VeFi Foundation"
+      meta={[
+        {
+          name: "description",
+          content:
+            "The VEFI Foundation is an independent standard body that controls and oversees the advancement of the VEFI Network and ecosystem. We are the owner of the VEFI brand. The foundation works to drive the adoption and partnerships, grow the VEFI community."
+        }
+      ]}
+    />
     <BrowserRouter>
       <App />
     </BrowserRouter>
