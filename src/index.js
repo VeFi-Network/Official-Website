d9ae62826ea00106c774d4d60b7a9ea182720b48
Kingsley Victor <javaprodigy56@gmail.com>
2021-10-31 18:50:27 +0100

    add Helmet for SEO

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import App from "./App";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+const MainComponent = () => (
+  <div>
+    <Helmet>
+      <meta
+        name="description"
+        content="The VEFI Foundation is an independent standard body that controls and oversees the advancement of the VEFI Network and ecosystem. We are the owner of the VEFI brand. The foundation works to drive the adoption and partnerships, grow the VEFI community."
+      />
+      <title>The VEFI Foundation</title>
+    </Helmet>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </div>
 );
+
+ReactDOM.render(<MainComponent />, document.getElementById("root"));
